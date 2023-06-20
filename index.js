7bf0bb786b3e4a7214509f863569d28fb356ff8e
Emmanuel <emwanzia532@gmail.com>
2023-06-21 00:20:27 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (a, b) => {
+
+    return 2000/100;
+}
+ console.log(divide(2000/1000));
+
+ const square = (num) => {
+    return num * num;
+ }
+ console.log(square(5));
+
+const add = (a, b) => {
+    return a + b;
+}
+console.log(add(5, 3));
